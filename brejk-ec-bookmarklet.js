fafce45fe7ee409761db22ee7bc115df903d108b
Jan Heřman <22095056+janherman@users.noreply.github.com>
2022-01-22 10:04:53 +0100

    Řešení včetně započítání sehranosti

--- brejk-ec-bookmarklet.js
+++ brejk-ec-bookmarklet.js
@@ -3,7 +3,9 @@
 
   if (parametrStranky && parametrStranky == "tym_kadr") {
     const kontejner = document.querySelector(".content-data");
-    //const nominace = document.querySelectorAll("input[id^='checkbox_']");
+    // Pokud je sehranost na desetinné místo, parseInt to pravděpodobně zaokrouhlí, resp. na desetinné místo nebere ohled. Opravit?
+    let sehranost = parseInt(document.querySelector("#div_sehranost td:nth-child(2)").textContent.slice(10, -2));
+    let sehranostBonusKoeficient = (((40 * sehranost) / 100) / 100) + 1;
     let ovladaciPanel = document.createElement("div");
     let toggle = document.createElement("input");
     let toggleLabel = document.createElement("label");
@@ -11,12 +13,16 @@
       let radky = document.querySelectorAll(
         "[id^='div_kategorie_id'] > table > tbody > tr"
       );
+      sehranost = parseInt(document.querySelector("#div_sehranost td:nth-child(2)").textContent.slice(10, -2));
+      sehranostBonusKoeficient = (((40 * sehranost) / 100) / 100) + 1;
       
-      radky.forEach(function (g) {
-        let ec = parseInt(g.querySelector("td:nth-child(22)").innerHTML) / 100;
-        let atributy = g.querySelectorAll("td");
+      radky.forEach(function(r) {
+        let nominovany = r.classList.contains("bgcolor-orange");
+        let ec = parseInt(r.querySelector("td:nth-child(22)").innerHTML) / 100;
+        let atributy = r.querySelectorAll("td");
 
         for (let index = 3; index < 12; index++) {
+
           if (atributy[index].querySelector("span")) {
             atributy[index].removeChild(
               atributy[index].querySelector("span")
@@ -30,9 +36,17 @@
             nalepka.style.width = "100%";
             nalepka.style.textAlign = "right";
             nalepka.style.color = "#000";
-            nalepka.innerHTML = Math.round(
-              parseInt(atributy[index].innerHTML) * ec
-            );
+
+            if(nominovany) {
+              nalepka.innerHTML = Math.round(
+                parseInt(atributy[index].innerHTML) * ec * sehranostBonusKoeficient
+              );
+            } else {
+              nalepka.innerHTML = Math.round(
+                parseInt(atributy[index].innerHTML) * ec
+              );
+            }
+
 
             atributy[index].style.position = "relative";
             atributy[index].style.color = "transparent";
@@ -48,9 +62,10 @@
         }
       });
     };
+
     toggle.type = "checkbox";
     toggle.id = "zapnout-ec-zobrazeni";
-    toggleLabel.innerHTML = "Zobrazit atributy dle aktuální Ec";
+    toggleLabel.innerHTML = "Zobrazit atributy dle aktuální Ec a sehranosti";
     toggleLabel.setAttribute("for", "zapnout-ec-zobrazeni");
     toggleLabel.style.marginRight = "1rem";
     ovladaciPanel.style.padding = "1rem";
@@ -61,10 +76,6 @@
 
     kontejner.prepend(ovladaciPanel);
 
-    // toggle.addEventListener("change", function() {
-    //   prepsatBunky();
-    // });
-
     kontejner.addEventListener("change", function(e) {
       if(e.target.getAttribute("type") == "checkbox" || e.target.getAttribute("type") == "select") {
         setInterval(function() {
@@ -73,12 +84,5 @@
       }
     });
 
-    // nominace.forEach(function (n) {
-    //   n.addEventListener("change", function () {
-    //     console.log("Změna nominace");
-    //     nominace = document.querySelectorAll("input[id^='checkbox_']");
-    //     prepsatBunky();
-    //   });
-    // });
   }
 })();
